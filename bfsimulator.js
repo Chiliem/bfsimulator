ca8fea372a0c67d8e2955e8a8b53392d958f83d0
Chiliem <37306303+Chiliem@users.noreply.github.com>
2026-02-03 21:46:16 -0500

    Add files via upload

---
+++ bfsimulator.js
@@ -0,0 +1,22 @@
+const stage = document.getElementById("stage");
+
+const img = document.getElementById("bfImg");
+
+
+
+const freeInput = document.getElementById("freeInput");
+
+let lastUserInput = "";
+
+freeInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+
+    lastUserInput = freeInput.value.trim();
+    if (!lastUserInput) return;
+
+    console.log("User submitted:", lastUserInput);
+
+    freeInput.value = ""; // clear after send
+  }
+});
